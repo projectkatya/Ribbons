403a4b903526029208570714eb730450d848540b
projectkatya <devteam@projectkatya.com>
2023-07-12 07:15:29 +0800

    Initial commit

---
+++ Ribbons/CryptographyExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace Ribbons
+{
+    public static class CryptographyExtensions
+    {
+        public static byte[] HashWithMD5(this string str)
+        {
+            return str.ComputeHash<MD5>();
+        }
+
+        public static byte[] HashWithSHA1(this string str)
+        {
+            return str.ComputeHash<SHA1>();
+        }
+
+        public static byte[] HashWithSHA256(this string str)
+        {
+            return str.ComputeHash<SHA256>();
+        }
+
+        public static byte[] HashWithSHA384(this string str)
+        {
+            return str.ComputeHash<SHA384>();
+        }
+
+        public static byte[] HashWithSHA512(this string str)
+        {
+            return str.ComputeHash<SHA512>();
+        }
+
+        private static byte[] ComputeHash<THashAlgorithm>(this string str) where THashAlgorithm : HashAlgorithm
+        {
+            HashAlgorithm hashAlgorithm = HashAlgorithm.Create(typeof(THashAlgorithm).Name);
+            return hashAlgorithm.ComputeHash(str.UTF8Bytes());
+        }
+    }
+}
---
+++ Ribbons/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Ribbons
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        public static byte[] UTF7Bytes(this string str)
+        {
+            return Encoding.UTF7.GetBytes(str);
+        }
+
+        public static byte[] UTF8Bytes(this string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        public static byte[] UTF32Bytes(this string str)
+        {
+            return Encoding.UTF32.GetBytes(str);
+        }
+
+        public static byte[] ASCIIBytes(this string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+    }
+}
