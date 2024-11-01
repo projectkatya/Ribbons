88a4b091490fbd91d5f3697dd7c990d4f651052d
projectkatya <devteam@projectkatya.com>
2024-11-02 00:13:24 +0800

    Add serialization library

---
+++ Ribbons.Serialization/JsonExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ribbons.Serialization
+{
+    internal class JsonExtensions
+    {
+    }
+}
