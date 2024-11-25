252716baf266f3feae2f255cc694f37ebd677123
projectkatya <devteam@projectkatya.com>
2024-11-25 22:35:09 +0800

    Checkpoint commit

---
+++ Ribbons.Users/UserAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Ribbons.Users
+{
+    public class UserAttribute
+    {
+        public long UserAttributeId { get; set; }
+        public long UserId { get; set; }
+        public long UserAttributeTypeId { get; set; }
+    }
+}
---
+++ Ribbons.Users/UserAttributeType.cs
@@ -0,0 +1,12 @@
+﻿namespace Ribbons.Users
+{
+    public class UserAttributeType
+    {
+        public long UserAttributeTypeId { get; set; }
+        public long UserTypeId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ValueType { get; set; }
+    }
+}
