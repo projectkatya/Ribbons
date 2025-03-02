5c5b312c8f01b3826fc2462504752494652606fd
projectkatya <devteam@projectkatya.com>
2025-03-02 21:50:26 +0800

    Add baseline projects

---
+++ Ribbons.Data/Database.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ribbons.Data
+{
+    public abstract class Database : DbContext
+    {
+        protected DatabaseProvider Provider { get; }
+
+        protected Database(DatabaseProvider provider)
+        {
+            Provider = provider;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            switch (Provider)
+            {
+                case DatabaseProvider.MsSql:
+                    optionsBuilder.UseSqlServer();
+                    break;
+                case DatabaseProvider.MySql:
+                    optionsBuilder.UseMySQL();
+                    break;
+                case DatabaseProvider.Npgsql:
+                    optionsBuilder.UseNpgsql();
+                    break;
+                case DatabaseProvider.Oracle:
+                    optionsBuilder.UseOracle();
+                    break;
+                case DatabaseProvider.Sqlite:
+                    optionsBuilder.UseSqlite();
+                    break;
+            }
+        }
+    }
+}
---
+++ Ribbons/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Ribbons
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
